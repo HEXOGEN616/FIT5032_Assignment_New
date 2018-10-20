d04d1c009c62dc011e3007be354165ef1e047f0f
Kang Meng <kmen0004@student.monash.edu>
2018-10-20 13:21:47 +1100

    jumbotron - cover

--- FIT5032_Assignment_New/Controllers/PostsController.cs
+++ FIT5032_Assignment_New/Controllers/PostsController.cs
@@ -92,6 +92,8 @@ namespace FIT5032_Assignment_New.Controllers
             return View();
         }
 
+        
+
         // POST: Posts/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
